e07b420106417717753e4109e3206dd013ecd178
zemzale <alberts98@inbox.lv>
2016-10-17 23:58:34 +0300

    Added window manager (needs fixing)

---
+++ Assets/Scripts/WindowManager.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class WindowManager : MonoBehaviour {
+
+    private float offset;
+    private Vector3 cv;
+
+    private Vector3 gameWindowPos;
+    private Vector3 settingsWindowPos;
+
+    [SerializeField]
+    private float smoothTime = 1;
+
+    [SerializeField]
+    private RectTransform gameWindow;
+    [SerializeField]
+    private RectTransform settingsWindow;
+
+    [SerializeField]
+    private Button settingsButton;
+
+    [SerializeField]
+    private Button backToGameWindowSettingsButton;
+
+    void Start()
+    {
+        offset = gameWindow.rect.width;
+
+
+        settingsButton.onClick.AddListener(() => {
+            StartCoroutine(ChangeToSettings());
+        });
+
+        backToGameWindowSettingsButton.onClick.AddListener(() =>
+        {
+            StartCoroutine(ChangeToGame());
+        });
+    }
+
+
+    //Changes window with smooth effect. Nice meme.
+    IEnumerator ChangeToSettings()
+    {
+        Vector3 target1 = new Vector3(0f, 0f);
+        Vector3 target2 = new Vector3(offset, 0f);
+
+        while (settingsWindow.localPosition != target1)
+        {
+            //settingsWindow.localPosition = target1;
+            //gameWindow.localPosition = target2;
+
+            settingsWindow.localPosition = Vector3.SmoothDamp(settingsWindow.localPosition, target1, ref cv, smoothTime);
+            gameWindow.localPosition = Vector3.SmoothDamp(gameWindow.localPosition, target2, ref cv, smoothTime);
+            yield return null;
+        }
+    } 
+    IEnumerator ChangeToGame()
+    {
+        Vector3 target1 = new Vector3(0f, 0f);
+        Vector3 target2 = new Vector3(-offset, 0f);
+
+        while (gameWindow.localPosition != target1)
+        {
+            
+            //gameWindow.localPosition = target1;
+            //settingsWindow.localPosition = target2;
+
+            gameWindow.localPosition = Vector3.SmoothDamp(gameWindow.localPosition, target1, ref cv, smoothTime);
+            settingsWindow.localPosition = Vector3.SmoothDamp(settingsWindow.localPosition, target2, ref cv, smoothTime);
+            yield return null;
+        }
+    }
+}
