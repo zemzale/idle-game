3f6128bf5ebc75a571771227c89e75d966ac8de3
zemzale <alberts98@inbox.lv>
2016-10-18 23:15:32 +0300

    Fixed animation lerping!

--- Assets/Scripts/WindowManager.cs
+++ Assets/Scripts/WindowManager.cs
@@ -5,13 +5,10 @@ using System.Collections;
 public class WindowManager : MonoBehaviour {
 
     private float offset;
-    private Vector3 cv;
-
-    private Vector3 gameWindowPos;
-    private Vector3 settingsWindowPos;
+    
 
     [SerializeField]
-    private float smoothTime = 1;
+    private float lerpTime = 1;
 
     [SerializeField]
     private RectTransform gameWindow;
@@ -28,48 +25,52 @@ public class WindowManager : MonoBehaviour {
     {
         offset = gameWindow.rect.width;
 
-
         settingsButton.onClick.AddListener(() => {
-            StartCoroutine(ChangeToSettings());
+            StartCoroutine(ChangeToWindows(gameWindow, settingsWindow, true));
         });
 
         backToGameWindowSettingsButton.onClick.AddListener(() =>
         {
-            StartCoroutine(ChangeToGame());
+            StartCoroutine(ChangeToWindows(settingsWindow, gameWindow, false));
         });
     }
 
 
+
+    
+    
+
     //Changes window with smooth effect. Nice meme.
-    IEnumerator ChangeToSettings()
+    IEnumerator ChangeToWindows(RectTransform curWin, RectTransform tarWin, bool right)
     {
-        Vector3 target1 = new Vector3(0f, 0f);
-        Vector3 target2 = new Vector3(offset, 0f);
 
-        while (settingsWindow.localPosition != target1)
+        float currentLerpTime = 0f;
+        float dir;
+        if (right)
+            dir = offset;
+        else
+            dir = -offset;
+
+        Vector3 start1 = curWin.localPosition;
+        Vector3 start2 = tarWin.localPosition;
+
+        Vector3 target1 = new Vector3(dir, 0f);
+        Vector3 target2 = new Vector3(0f, 0f);
+        
+
+        while (tarWin.localPosition != target2)
         {
-            //settingsWindow.localPosition = target1;
-            //gameWindow.localPosition = target2;
+            currentLerpTime += Time.deltaTime;
+            if (currentLerpTime > lerpTime)
+            {
+                currentLerpTime = lerpTime;
+            }
 
-            settingsWindow.localPosition = Vector3.SmoothDamp(settingsWindow.localPosition, target1, ref cv, smoothTime);
-            gameWindow.localPosition = Vector3.SmoothDamp(gameWindow.localPosition, target2, ref cv, smoothTime);
+            float perc = currentLerpTime / lerpTime;
+            curWin.localPosition = Vector3.Lerp(start1, target1, perc);
+            tarWin.localPosition = Vector3.Lerp(start2, target2, perc);
             yield return null;
         }
+        
     } 
-    IEnumerator ChangeToGame()
-    {
-        Vector3 target1 = new Vector3(0f, 0f);
-        Vector3 target2 = new Vector3(-offset, 0f);
-
-        while (gameWindow.localPosition != target1)
-        {
-            
-            //gameWindow.localPosition = target1;
-            //settingsWindow.localPosition = target2;
-
-            gameWindow.localPosition = Vector3.SmoothDamp(gameWindow.localPosition, target1, ref cv, smoothTime);
-            settingsWindow.localPosition = Vector3.SmoothDamp(settingsWindow.localPosition, target2, ref cv, smoothTime);
-            yield return null;
-        }
-    }
 }
