31dff62739ae52720930e877842019c0f4534187
zemzale <alberts98@inbox.lv>
2016-10-20 21:56:40 +0300

    Made stage class and contorls for it.

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,25 +1,45 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class LevelManager : MonoBehaviour {
 
     public static LevelManager singelton;
 
     [SerializeField]
-    private string[] trainingStage = new string[5];
+    private Stage[] stages;
+    private Stage currentStage; 
 
     private int currentIndx = 0;
 
     void Start ()
     {
         singelton = this;
+        NextStage();
     }
 
+    private void NextStage()
+    {
+        //TODO: CHECK FOR OUT OF INDEX ERROR.
+        currentStage = stages[currentIndx];
+        currentIndx++;
+    }
+
+    //Karoča tu paņem enemy no tagadeja stage.
+    //Ja tu kruts un visus nositi dabū jauno stage. 
+    //And ez lyfe.
     public string NextEnemy()
     {
-        if (currentIndx == trainingStage.Length - 1)
-            currentIndx = 0;
-        Debug.Log("Feeding Next enemy!");
-        currentIndx++;
-        return trainingStage[currentIndx - 1];
+        string tmp = currentStage.NextEnemy();
+        Debug.LogWarning("Geting next enemy!" + tmp);
+        if (tmp.Equals("DONE") == true)
+        {
+            NextStage();
+            NextEnemy();
+            return null;
+        }
+        else
+            return tmp;
     }
+
+    
 }
